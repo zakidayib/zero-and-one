6375f22df6b707871588d5da7a5255ba80179256
zaki tayyip <zakidayib1997@gmail.com>
2023-06-13 23:53:47 +0300

    zero-one_solved

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+function ZeroAndOne(arr){
+    let newArr = [];
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]=== "Zero"){
+            newArr.push(0);
+        }
+        else if(arr[i]==="One"){
+            newArr.push(1);
+        }
+    }
+
+    return newArr.join("");
+
+}
+
+
+
+console.log(ZeroAndOne(["Zero", "ONE", "one", "Zero"]));
